aed1b44d98089bbc0181491b66bf5f2ef30077dd
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-07-24 21:20:00 -0500

    Implement Notification System

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:get_it/get_it.dart';
+import 'services/plant_service.dart';
+import 'services/notification_service.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+GetIt locator = GetIt.instance;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  debugPrint('Settings up services');
+  setupServices();
+  debugPrint('done setting up services');
+  tz.initializeTimeZones();
   runApp(PlantWateringApp());
+  requestNotificationPermissions();
+}
+
+void setupServices() {
+  locator.registerSingleton(PlantService());
+  locator.registerSingleton(NotificationService());
+}
+
+void requestNotificationPermissions() async {
+  var status = await Permission.notification.status;
+  debugPrint(status.toString());
+  if (!status.isGranted) {
+    status = await Permission.notification.request();
+    if (!status.isGranted) {
+      // The user did not grant the permission. Handle this case appropriately.
+    }
+  }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const appName = 'Plant Watering Tracker';
+  static const primaryAppColor = Colors.green;
+  static const secondaryAppColor = Colors.greenAccent;
+  static const defaultPadding = 16.0;
+}
