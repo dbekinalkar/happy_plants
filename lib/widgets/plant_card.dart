39acbe650fa92284ffbc06769f1977edd22c3542
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-07-24 21:19:27 -0500

    Necessary Components

---
+++ lib/widgets/plant_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../models/plant_model.dart';
+import '../screens/plant_detail_screen.dart';
+import '../utils/utils.dart';
+
+// In PlantCard
+class PlantCard extends StatefulWidget {
+  final Plant plant;
+
+  PlantCard({required this.plant});
+
+  @override
+  _PlantCardState createState() => _PlantCardState();
+}
+
+class _PlantCardState extends State<PlantCard> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(minutes: 1), (Timer t) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(widget.plant.name),
+        subtitle: Text(
+            'Next watering in: ${formatDuration(widget.plant.nextWateringTime.difference(DateTime.now()))}'),
+        trailing: Icon(Icons.chevron_right),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PlantDetailScreen(plant: widget.plant)),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Similar changes for PlantDetailScreen
